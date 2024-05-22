d53fde7e7cafef17d96d09f9b0aea11c453998bd
Koji Adriano <iisparkplugiiog@gmail.com>
2024-05-23 01:28:44 +0800

    Update cors allowed and clear db

--- ApiRoot/ApiRoot/settings.py
+++ ApiRoot/ApiRoot/settings.py
@@ -68,6 +68,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://frontend-lac-nine-49.vercel.app",
+    "https://blogsite-fe.vercel.app"
 ]
 
 ROOT_URLCONF = 'ApiRoot.urls'
