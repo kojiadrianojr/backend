76ebfda45bdae32418fc27e8f1523d80aa1bb9c4
Koji Adriano <iisparkplugiiog@gmail.com>
2024-05-24 22:59:29 +0800

    Update models, and clear DB

--- ApiRoot/blog/models.py
+++ ApiRoot/blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Blog(models.Model):
   owner = models.CharField(max_length=255)
   title = models.CharField(max_length=255)
-  imageUrl = models.CharField(max_length=255, default=" ")
+  imageUrl = models.CharField(max_length=255, blank=True)
   description = models.TextField()
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
